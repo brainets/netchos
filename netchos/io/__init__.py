cbb72845ede3580375d18df796b56191f83cdb4b
EtienneCmb <e.combrisson@gmail.com>
2021-04-21 11:28:57 +0200

    rm verbose

--- netchos/io/__init__.py
+++ netchos/io/__init__.py
@@ -1,4 +1,4 @@
 """I/O conversion functions."""
 from .io_convert import io_to_df  # noqa
 from .io_mpl_to_px import mpl_to_px_inputs  # noqa
-from .io_syslog import set_log_level, logger, verbose  # noqa
+from .io_syslog import set_log_level, logger  # noqa
